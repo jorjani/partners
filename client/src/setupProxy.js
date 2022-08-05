68a27dc59f4108d91835264f35bfb8a560b3c9b0
Tevan Bhatia <45053368+TevanB@users.noreply.github.com>
2022-08-05 15:18:38 -0400

    Client Only push to heroku

---
+++ client/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+      'http://localhost:5000',
+      createProxyMiddleware({
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      })
+  );
+};
