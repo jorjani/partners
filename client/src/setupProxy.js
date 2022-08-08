2ec7455ae75bb4c6b40aae2e017b893d1a3cf141
Tevan Bhatia <45053368+TevanB@users.noreply.github.com>
2022-08-08 14:43:03 -0400

    Changing API URL to dev version

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-      'http://localhost:5000/api',
+      '/api',
       createProxyMiddleware({
         target: 'http://localhost:5000/api',
         changeOrigin: true,
