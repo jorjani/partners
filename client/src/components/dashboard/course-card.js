7c6a0167e39dd13edeb2def81aca4525d3d1bf2d
Tevan Bhatia <45053368+TevanB@users.noreply.github.com>
2022-08-20 18:16:02 -0400

    Course Card navigation for SSR fixed

--- client/src/components/dashboard/course-card.js
+++ client/src/components/dashboard/course-card.js
@@ -1,53 +1,51 @@
 import { Box, Card, CardContent, Grid, Typography } from '@mui/material';
-import { useState } from 'react';
+import NextLink from 'next/link';
 export const CourseCard = (props) => {
-    const navigate = (id) => {
-        window.location.href = `/course/${id}/overview`;
-    }
     return (
         <Card
-            onClick={() => navigate(props.course_id)}
             sx={{ height: '100%', marginLeft: '10px', marginRight: '10px' }}
             {...props}
         >
-            <CardContent>
-                <Grid
-                    container
-                    spacing={3}
-                    sx={{ justifyContent: 'space-between' }}
-                >
-                    <Grid item>
+            <NextLink href={`/course/${props.course_id}/overview`} passHref>
+                <CardContent>
+                    <Grid
+                        container
+                        spacing={3}
+                        sx={{ justifyContent: 'space-between' }}
+                    >
+                        <Grid item>
+                            <Typography
+                                color="textSecondary"
+                                gutterBottom
+                                variant="overline"
+                            >
+                                {props.course_organization}
+                            </Typography>
+                            <Typography
+                                color="textPrimary"
+                                variant="h4"
+                            >
+                                {props.course_code}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    <Box
+                        sx={{
+                            pt: 2,
+                            display: 'flex',
+                            alignItems: 'center'
+                        }}
+                    >
                         <Typography
                             color="textSecondary"
-                            gutterBottom
-                            variant="overline"
+                            variant="caption"
+                            ml={1}
                         >
-                            {props.course_organization}
+                            Projects: {props.course_projects}
                         </Typography>
-                        <Typography
-                            color="textPrimary"
-                            variant="h4"
-                        >
-                            {props.course_code}
-                        </Typography>
-                    </Grid>
-                </Grid>
-                <Box
-                    sx={{
-                        pt: 2,
-                        display: 'flex',
-                        alignItems: 'center'
-                    }}
-                >
-                    <Typography
-                        color="textSecondary"
-                        variant="caption"
-                        ml={1}
-                    >
-                        Projects: {props.course_projects}
-                    </Typography>
-                </Box>
-            </CardContent>
+                    </Box>
+                </CardContent>
+            </NextLink>
         </Card>
     )
 };
