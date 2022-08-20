6c02d84a78a82c8dcdd019c167069e768cbdcc0d
Tevan Bhatia <45053368+TevanB@users.noreply.github.com>
2022-08-20 19:05:22 -0400

    Restricted access to recieved projects

--- client/src/pages/course/[course_id]/project/[project_id]/overview.js
+++ client/src/pages/course/[course_id]/project/[project_id]/overview.js
@@ -70,85 +70,6 @@ const Project = () => {
       confirmButtonText: "OK",
     });
   };
-  // const invitePartners = () => {
-  //   Swal.fire({
-  //     title: `Invite Partners to ${getCourseFromURL().toUpperCase()}`,
-  //     html: `
-  //     <input type="text" id="org-info" class="swal2-input" placeholder="Organization Info">
-  //     <input type="text" id="email" class="swal2-input" placeholder="Email">
-  //     <input type="text" id="name" class="swal2-input" placeholder="Name">
-  //     <input type="text" id="info" class="swal2-input" placeholder="Organization Affiliation">`,
-  //     cancelButtonText: "Cancel",
-  //     confirmButtonText: "Invite",
-  //     showCancelButton: true,
-  //     focusConfirm: false,
-  //     preConfirm: () => {
-  //       const orgInfo = Swal.getPopup().querySelector("#org-info").value;
-  //       const email = Swal.getPopup().querySelector("#email").value;
-  //       const name = Swal.getPopup().querySelector("#name").value;
-  //       const info = Swal.getPopup().querySelector("#info").value;
-  //       let validationMessage = "";
-  //       if (!orgInfo) {
-  //         validationMessage += "Organization Info is required. ";
-  //       }
-  //       if (!validateEmail(email)) {
-  //         validationMessage += "Email address is not valid. ";
-  //       }
-  //       if (!name) {
-  //         validationMessage += "Name is required. ";
-  //       }
-  //       if (!info) {
-  //         validationMessage += "Affiliation Info is required. ";
-  //       }
-  //       if (validationMessage !== "") {
-  //         Swal.showValidationMessage(validationMessage);
-  //       }
-  //       return { email: email, subject: subject, message: message };
-  //     },
-  //   }).then((result) => {
-  //     if (result.isConfirmed) {
-  //       let invitationInfo = result.value;
-  //       Swal.fire("", "Your invitation has been sent.", "success");
-  //     }
-  //   });
-  // };
-  // const inviteStudents = () => {
-  //   Swal.fire({
-  //     title: `Invite Students to ${getCourseFromURL().toUpperCase()}`,
-  //     html: `Send via Email
-  //     <input type="text" id="email" class="swal2-input" placeholder="Email">
-  //     <input type="text" id="subject" class="swal2-input" placeholder="Subject">
-  //     <input type="text" id="message" class="swal2-input" placeholder="Message">`,
-  //     cancelButtonText: "Cancel",
-  //     confirmButtonText: "Invite",
-  //     showCancelButton: true,
-  //     focusConfirm: false,
-  //     preConfirm: () => {
-  //       const email = Swal.getPopup().querySelector("#email").value;
-  //       const subject = Swal.getPopup().querySelector("#subject").value;
-  //       const message = Swal.getPopup().querySelector("#message").value;
-  //       let validationMessage = "";
-  //       if (!validateEmail(email)) {
-  //         validationMessage += "Email address is not valid. ";
-  //       }
-  //       if (!subject) {
-  //         validationMessage += "Subject is required. ";
-  //       }
-  //       if (!message) {
-  //         validationMessage += "Message is required. ";
-  //       }
-  //       if (validationMessage !== "") {
-  //         Swal.showValidationMessage(validationMessage);
-  //       }
-  //       return { email: email, subject: subject, message: message };
-  //     },
-  //   }).then((result) => {
-  //     if (result.isConfirmed) {
-  //       let userInfo = result.value;
-  //       Swal.fire("", "Your invitation has been sent.", "success");
-  //     }
-  //   });
-  // };
   return (
     <>
       <Head>
@@ -172,18 +93,19 @@ const Project = () => {
               xs={9}>
               <NavPath />
             </Grid>
+            <Grid item
+              lg={4}
+              sm={4}
+              xl={4}
+              xs={4}>
+              <Typography color="textPrimary"
+                variant="h4">
+                {getProjectFromURL()}
+              </Typography>
+            </Grid>
             {userData.type === 'management' ? (
               <>
-                <Grid item
-                  lg={4}
-                  sm={4}
-                  xl={4}
-                  xs={4}>
-                  <Typography color="textPrimary"
-                    variant="h4">
-                    {getProjectFromURL()}
-                  </Typography>
-                </Grid>
+
                 <Grid item
                   mr={5}
                   lg={2}
